7a94e4e3208baf392ba8494e1a4997c0a5d4d361
whcksdid01-ui <whcksdid01@gmail.com>
2026-04-22 04:07:38 +0900

    Add DeviceSpoofingService with basic structure

---
+++ Services/DeviceSpoofingService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourNamespace.Services
+{
+    /// <summary>
+    /// Handles device spoofing techniques to avoid detection by fingerprinting methods.
+    /// </summary>
+    public class DeviceSpoofingService
+    {
+        // Implement WebGL, Canvas, WebRTC fingerprinting spoofing methods here
+    }
+}
